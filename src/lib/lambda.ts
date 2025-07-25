ef91ecacae5856dd9243d90675bf9184a209d20b
ValentinoTriadi <13522164@std.stei.itb.ac.id>
2025-07-25 15:07:28 +0700

    fix: add lambda credentials

--- src/lib/lambda.ts
+++ src/lib/lambda.ts
@@ -4,4 +4,8 @@ import { env } from '@/configs';
 
 export const lambda = new Lambda({
   region: env.AWS_REGION || 'us-east-1',
+  credentials: {
+    accessKeyId: env.AWS_ACCESS_KEY_ID || '',
+    secretAccessKey: env.AWS_SECRET_ACCESS_KEY || '',
+  },
 });
